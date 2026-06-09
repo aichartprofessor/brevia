527592c48fd4fc231f271136ab7e8b2a584bddda
aichartprofessor <kjy040423@gmail.com>
2026-06-09 23:45:45 +0900

    chat.js

--- api/chat.js
+++ api/chat.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
-    res.status(200).json(data);
+    res.setHeader('Content-Type', 'application/json'); res.status(200).json(data);;
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
